84f196218cfcbea3e98b686abb32bedc935db7c0
fukuiretu <s0232101@gmail.com>
2015-10-31 12:46:43 +0900

    Add 65

---
+++ 65.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int, 2)
+	c <- 1
+	c <- 2
+	c <- 3
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	// fatal error: all goroutines are asleep - deadlock!
+
+	// これならOK
+	// c <- 3
+	// fmt.Println(<-c)
+}
